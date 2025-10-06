85159389051c5889d30bbc340aacd20cc0c50a70
aqwenaaa <aqueenahapsari22@smk.belajar.id>
2025-10-07 00:49:11 +0700

    Langkah 3: Lengkapi Kode di main.dart

--- lib/pages/item_page.dart
+++ lib/pages/item_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class ItemPage extends StatelessWidget {
+  const ItemPage({super.key});
 
   @override
   Widget build(BuildContext context) {
